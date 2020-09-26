66324b8e6d59fac8fafa323716c317f8ddc5dd0e
Sho3366 <71325292+Sho3366@users.noreply.github.com>
2020-09-26 12:03:30 +0900

    update

---
+++ main.js
@@ -0,0 +1,27 @@
+//音声自動文字起こし
+
+//音声認識の処理はこの2行でOK
+//webkitSpeechRecognition()を定義し、
+//langをja-JPにすることで
+//日本語に対応したWeb Speech APIが使えるようになる
+const speech = new webkitSpeechRecognition();
+speech.lang = 'ja-JP';
+
+const btn = document.getElementById('btn');
+const content = document.getElementById('content');
+
+btn.addEventListener('click', function() {
+    speech.start();
+});
+
+speech.onresult = function(e) {
+    speech.stop();
+    if (e.results[0].isFinal) {
+        var autotext = e.results[0][0].transcript
+        content.innerHTML += '<div>'+ autotext +'</div>';
+    }
+}
+
+speech.onend = () => {
+    speech.start()
+};
